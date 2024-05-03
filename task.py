c58f8922c935796879547896ba905150da3335fe
Your Namehuseynabbasov <abbasovhuseyn670@gmail.com>
2024-05-03 08:31:22 -0700

    first commit

---
+++ task.py
@@ -0,0 +1,38 @@
+#Task 1 
+
+# newList = [] 
+# for i in range(0,5):
+#     i = int (input("Reqem daxil edin: "))
+#     newList.append(i)
+# print(f"Normal list : {newList}")
+# newList.sort()
+
+# print(f"Azdan coxa siralanan list: {newList}")
+
+
+#Task 2
+# word = input("Sozu daxil edin: ")
+# cvb = sorted(word)
+# print(f"Alfabetik sira {cvb}")
+
+#Task 3
+# n = 13
+# count = 0
+# while True:
+#     num = int(input("Reqem daxil edin: "))
+#     count += 1
+#     if num == n: 
+#         print(f"Tebrikler {count}-ci yoxlamada 13 reqemini daxil etdiniz ")
+#         break
+
+#Task 4 
+# sade_ededler = [] 
+# for eded in range(2, 101):
+#     sade = True   
+#     for i in range(2, int(eded ** 0.5) + 1):
+#         if eded % i == 0:   
+#             sade = False   
+#             break  
+#     if sade:
+#         sade_ededler.append(eded)    
+# print(f"1'den 100'e qeder olan sade ededler: \n \t{sade_ededler}") 
